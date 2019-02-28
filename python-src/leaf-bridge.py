bd54cf0b9b59c0a837a204095fca353464532a84
Sébastien MB <smassot@sierrawireless.com>
2019-02-28 16:28:39 +0100

    Adapt python code to PEP8
    Resolves: LETOOLS-757
    Change-Id: Iec9204053dfe476caa217a9bb240ecc0ab2333c0

--- python-src/leaf-bridge.py
+++ python-src/leaf-bridge.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python3
 # LEAF_DESCRIPTION vscode plugin interface
-'''
+"""
 Leaf Package Manager
 
 @author:    Legato Tooling Team <letools@sierrawireless.com>
 @copyright: Sierra Wireless. All rights reserved.
 @contact:   Legato Tooling Team <letools@sierrawireless.com>
 @license:   https://www.mozilla.org/en-US/MPL/2.0/
-'''
+"""
 
 import json
-import os
 import subprocess
 import sys
 import traceback
@@ -18,29 +17,27 @@ from abc import ABC, abstractmethod
 from pathlib import Path
 
 from leaf import __version__
-from leaf.constants import EnvConstants
+from leaf.api import PackageManager, RemoteManager, WorkspaceManager
+from leaf.core.constants import LeafSettings
 from leaf.core.error import WorkspaceNotInitializedException
-from leaf.core.packagemanager import PackageManager, RemoteManager
-from leaf.core.tags import TagManager
-from leaf.core.workspacemanager import WorkspaceManager
-from leaf.format.logger import Verbosity
-
-
-LEAF_DEBUG = os.getenv(EnvConstants.DEBUG_MODE, "") != ""
+from leaf.core.jsonutils import jloads
+from leaf.model.environment import Environment
+from leaf.model.tags import TagUtils
 
 
 class LeafHandler(ABC):
-    '''
+    """
     Generic class to define handlers
-    '''
-    def _getWorkspace(self, **kwargs):
-        out = kwargs['workspace']
+    """
+
+    def get_ws_folder(self, **kwargs):
+        out = kwargs["workspace"]
         if out is None:
             raise ValueError("Missing workspace in request")
         return Path(out)
 
     @abstractmethod
-    def getName(self):
+    def get_name(self):
         pass
 
     @abstractmethod
@@ -49,103 +46,103 @@ class LeafHandler(ABC):
 
 
 class RemotesHandler(LeafHandler):
-    '''
+    """
     Input Payload:
     {
         "command": "remotes"
     }
-    '''
+    """
 
-    def getName(self):
+    def get_name(self):
         return "remotes"
 
     def execute(self, **kwargs):
-        rm = RemoteManager(Verbosity.QUIET)
+        rm = RemoteManager()
         out = {}
-        for alias, remote in rm.listRemotes().items():
+        for alias, remote in rm.list_remotes().items():
             out[alias] = remote.json
-            if remote.isFetched():
-                out[alias]['info'] = remote.getInfo()
+            if remote.is_fetched:
+                out[alias]["info"] = remote.info_node
         return out
 
 
 class PackagesHandler(LeafHandler):
-    '''
+    """
     Input Payload:
     {
-        "command": "packages"
+        "command": "packages",
         "args": {
             "skipInstalled": true, // Optional, default false
             "skipAvailable": true // Optional, default false
         }
     }
-    '''
+    """
 
-    def getName(self):
+    def get_name(self):
         return "packages"
 
     def execute(self, **kwargs):
-        pm = PackageManager(Verbosity.QUIET)
-        ipMap = None
-        apMap = None
-        if 'skipInstalled' not in kwargs or not kwargs['skipInstalled']:
-            ipMap = pm.listInstalledPackages()
-        if 'skipAvailable' not in kwargs or not kwargs['skipAvailable']:
-            apMap = pm.listAvailablePackages()
-        
+        pm = PackageManager()
+        ipmap = None
+        apmap = None
+        if "skipInstalled" not in kwargs or not kwargs["skipInstalled"]:
+            ipmap = pm.list_installed_packages()
+        if "skipAvailable" not in kwargs or not kwargs["skipAvailable"]:
+            apmap = pm.list_available_packages()
+
         # if we have install & available packages, tag packages:
-        if ipMap is not None and apMap is not None:
-            mfList = list(ipMap.values()) + list(apMap.values())
-            TagManager().tagLatest(mfList)
-            TagManager().tagInstalled(apMap.values(), ipMap)
+        if ipmap is not None and apmap is not None:
+            mflist = list(ipmap.values()) + list(apmap.values())
+            TagUtils.tag_latest(mflist)
+            TagUtils.tag_installed(apmap.values(), ipmap.keys())
         out = {}
         # Build the output list
-        if ipMap is not None:
-            out['installedPackages'] = {}
-            for pi, ip in ipMap.items():
+        if ipmap is not None:
+            out["installedPackages"] = {}
+            for pi, ip in ipmap.items():
                 data = ip.json
-                data['folder'] = str(ip.folder)
-                data['info']['customTags'] = ip.customTags
-                out['installedPackages'][str(pi)] = data
-        if apMap is not None:
-            out['availablePackages'] = {}
-            for pi, ap in apMap.items():
+                data["folder"] = str(ip.folder)
+                data["info"]["customTags"] = ip.custom_tags
+                out["installedPackages"][str(pi)] = data
+        if apmap is not None:
+            out["availablePackages"] = {}
+            for pi, ap in apmap.items():
                 data = ap.json
-                data['remoteUrl'] = ap.remoteUrl
-                data['info']['customTags'] = ap.customTags
-                out['availablePackages'][str(pi)] = data
+                data["remoteUrl"] = ap.url
+                data["info"]["customTags"] = ap.custom_tags
+                out["availablePackages"][str(pi)] = data
         return out
 
 
 class WorkspaceHandler(LeafHandler):
-    '''
+    """
     Input Payload:
     {
         "command": "workspaceInfo",
         "workspace": "/path/to/workspace"
     }
-    '''
+    """
 
-    def getName(self):
+    def get_name(self):
         return "workspaceInfo"
 
     def execute(self, **kwargs):
-        wm = WorkspaceManager(self._getWorkspace(**kwargs), Verbosity.QUIET)
+        wm = WorkspaceManager(self.get_ws_folder(**kwargs))
         out = {}
-        out['rootFolder'] = str(wm.workspaceRootFolder)
-        if wm.isWorkspaceInitialized():
-            out['initialized'] = True
-            out['profiles'] = {}
-            for pfName, pf in wm.listProfiles().items():
-                out['profiles'][pfName] = pf.json
-                out['profiles'][pfName]['current'] = pf.isCurrentProfile
+        out["rootFolder"] = str(wm.ws_root_folder)
+        if wm.is_initialized:
+            out["initialized"] = True
+            out["profiles"] = {}
+            for pfname, pf in wm.list_profiles().items():
+                out["profiles"][pfname] = pf.json
+                out["profiles"][pfname]["current"] = pf.is_current
         else:
-            out['initialized'] = False
+            out["initialized"] = False
         return out
 
 
 class VariablesHandler(LeafHandler):
-    '''
+    """
     Input Payload:
     {
         "command": "resolveVariables",
@@ -155,74 +152,73 @@ class VariablesHandler(LeafHandler):
             "keys": ["optional", "keys"]
         }
     }
-    '''
+    """
 
-    def getName(self):
+    def get_name(self):
         return "resolveVariables"
 
     def execute(self, **kwargs):
         out = {}
-        wm = WorkspaceManager(self._getWorkspace(**kwargs), Verbosity.QUIET)
-        if not wm.isWorkspaceInitialized():
+        wm = WorkspaceManager(self.get_ws_folder(**kwargs))
+        if not wm.is_initialized:
             raise WorkspaceNotInitializedException()
-        profile = wm.getProfile(kwargs['profile'] if 'profile' in kwargs else wm.getCurrentProfileName())
-        wm.isProfileSync(profile, raiseIfNotSync=True)
-        exports = ""
-        profileEnv = wm.getFullEnvironment(profile)
-        exports = "; ".join(map(lambda kv: 'export %s="%s"' % kv, profileEnv.toList()))
-        lines = subprocess.check_output(["sh", "-c", exports + ' && env']).decode().splitlines()
+        profile = wm.get_profile(kwargs["profile"] if "profile" in kwargs else wm.current_profile_name)
+        wm.is_profile_sync(profile, raise_if_not_sync=True)
 
-        def findValue(key, lines):
+        pf_env = wm.build_full_environment(profile)
+        command = ""
+        for key, value in pf_env.tolist():
+            command += Environment.tostring_export(key, value)
+        command += " env"
+
+        lines = subprocess.check_output(["sh", "-c", command]).decode().splitlines()
+
+        def find_value(key, lines):
             for line in lines:
                 if line.startswith(key + "="):
-                    return line[len(key) + 1:]
+                    return line[len(key) + 1 :]
 
-        for k in kwargs.get('keys', profileEnv.keys()):
-            out[k] = findValue(k, lines)
+        for k in kwargs.get("keys", pf_env.keys()):
+            out[k] = find_value(k, lines)
         return out
 
 
 class InfoHandler(LeafHandler):
-    '''
+    """
     Input Payload:
     {
         "command": "info"
     }
-    '''
+    """
 
-    def getName(self):
+    def get_name(self):
         return "info"
 
     def execute(self, **kwargs):
         out = {}
-        pm = PackageManager(Verbosity.QUIET)
-        out['version'] = __version__
-        out['configFolder'] = str(pm.configurationFolder)
-        out['cacheFolder'] = str(pm.cacheFolder)
-        out['packageFolder'] = str(pm.getInstallFolder())
+        pm = PackageManager()
+        out["version"] = __version__
+        out["configFolder"] = str(pm.configuration_folder)
+        out["cacheFolder"] = str(pm.cache_folder)
+        out["packageFolder"] = str(pm.get_install_folder())
         return out
 
 
-def sendResponse(requestId, result=None, error=None):
+def send_response(request_id, result=None, error=None):
     out = {}
-    if requestId is not None:
-        out["id"] = requestId
+    if request_id is not None:
+        out["id"] = request_id
     if error is not None:
-        out["error"] = {'type': type(error).__name__,
-                        'message': str(error)}
+        out["error"] = {"type": type(error).__name__, "message": str(error)}
     elif result is not None:
         out["result"] = result
     print(json.dumps(out), flush=True)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     handlers = {}
-    for handler in (InfoHandler(),
-                    RemotesHandler(),
-                    PackagesHandler(),
-                    WorkspaceHandler(),
-                    VariablesHandler()):
-        handlers[handler.getName()] = handler
+    for handler in (InfoHandler(), RemotesHandler(), PackagesHandler(), WorkspaceHandler(), VariablesHandler()):
+        handlers[handler.get_name()] = handler
 
     for line in sys.stdin:
         line = line.strip()
@@ -233,26 +229,25 @@ if __name__ == '__main__':
         # Ignore blank lines
         if line != "":
             try:
-                jsonObject = json.loads(line)
-                requestId = jsonObject.get("id")
-                requestCommand = jsonObject.get("command")
-                requestWorkspace = jsonObject.get("workspace")
-                requestArgs = jsonObject.get("args", {})
+                json_data = jloads(line)
+                request_id = json_data.get("id")
+                request_command = json_data.get("command")
+                request_workspace = json_data.get("workspace")
+                request_args = json_data.get("args", {})
                 try:
-                    if requestCommand is None:
+                    if request_command is None:
                         raise ValueError("Missing command in request")
-                    if requestCommand not in handlers:
-                        raise ValueError("Unknown command: %s" % requestCommand)
-                    handler = handlers[requestCommand]
-                    sendResponse(requestId,
-                                 result=handler.execute(workspace=requestWorkspace, **requestArgs))
+                    if request_command not in handlers:
+                        raise ValueError("Unknown command: {command}".format(command=request_command))
+                    handler = handlers[request_command]
+                    send_response(request_id, result=handler.execute(workspace=request_workspace, **request_args))
                 except Exception as e:
-                    sendResponse(requestId, error=e)
-                    if LEAF_DEBUG:
+                    send_response(request_id, error=e)
+                    if LeafSettings.DEBUG_MODE.as_boolean():
                         traceback.print_exc(file=sys.stderr)
                         sys.stderr.flush()
             except Exception as e:
-                print("Invalid leaf request: %s (%s)" % (line, e), flush=True)
-                if LEAF_DEBUG:
+                print("Invalid leaf request: {line} ({error})".format(line=line, error=e))
+                if LeafSettings.DEBUG_MODE.as_boolean():
                     traceback.print_exc(file=sys.stderr)
                     sys.stderr.flush()
--- python-src/test/test-bridge.py
+++ python-src/test/test-bridge.py
@@ -13,127 +13,126 @@ LCI_BIN = ROOT_FOLDER = Path(__file__).parent.parent / "leaf-bridge.py"
 
 
 COMMAND_EXIT = "exit\n"
-COMMAND_INFO = '{"command":"info"}\n'
-COMMAND_REMOTES = '{"command":"remotes"}\n'
-COMMAND_PACKAGES = '{"command":"packages"}\n'
-COMMAND_PACKAGES_INSTALLED = '{"command":"packages","args":{"skipAvailable":true}}\n'
-COMMAND_PACKAGES_AVAILABLE = '{"command":"packages","args":{"skipInstalled":true}}\n'
-COMMAND_WORKSPACE = '{"command":"workspaceInfo"}\n'
-COMMAND_WORKSPACE__ws = '{"command":"workspaceInfo","workspace":"%s"}\n'
-COMMAND_VARIABLES__ws = '{"command":"resolveVariables","workspace":"%s"}\n'
-COMMAND_VARIABLES__ws_var = '{"command":"resolveVariables","workspace":"%s","args":{"keys":["%s"]}}\n'
-COMMAND_VARIABLES__ws_name = '{"command":"resolveVariables","workspace":"%s","args":{"profile":"%s"}}\n'
+COMMAND_INFO = '{{"command":"info"}}\n'
+COMMAND_REMOTES = '{{"command":"remotes"}}\n'
+COMMAND_PACKAGES = '{{"command":"packages"}}\n'
+COMMAND_PACKAGES_INSTALLED = '{{"command":"packages","args":{{"skipAvailable":true}}}}\n'
+COMMAND_PACKAGES_AVAILABLE = '{{"command":"packages","args":{{"skipInstalled":true}}}}\n'
+COMMAND_WORKSPACE = '{{"command":"workspaceInfo"}}\n'
+COMMAND_WORKSPACE__ws = '{{"command":"workspaceInfo","workspace":"{ws}"}}\n'
+COMMAND_VARIABLES__ws = '{{"command":"resolveVariables","workspace":"{ws}"}}\n'
+COMMAND_VARIABLES__ws_var = '{{"command":"resolveVariables","workspace":"{ws}","args":{{"keys":["{key}"]}}}}\n'
+COMMAND_VARIABLES__ws_name = '{{"command":"resolveVariables","workspace":"{ws}","args":{{"profile":"{key}"}}}}\n'
 
 
 class TestLeafCodeInterface(unittest.TestCase):
-
-    def testInfo(self):
+    def test_info(self):
         with Popen([str(LCI_BIN)], stdout=subprocess.PIPE, stdin=subprocess.PIPE) as proc:
-            outs, errs = proc.communicate(input=COMMAND_INFO.encode())
+            outs, errs = proc.communicate(input=COMMAND_INFO.format().encode())
             result = json.loads(outs.decode())
-            self.assertIsNone(result.get('error'))
-            self.assertEqual(__version__, result['result']['version'])
-            self.assertTrue(Path(result['result']['packageFolder']).is_dir())
-            self.assertTrue(Path(result['result']['configFolder']).is_dir())
-            self.assertTrue(Path(result['result']['cacheFolder']).is_dir())
+            self.assertIsNone(result.get("error"))
+            self.assertEqual(__version__, result["result"]["version"])
+            self.assertTrue(Path(result["result"]["packageFolder"]).is_dir())
+            self.assertTrue(Path(result["result"]["configFolder"]).is_dir())
+            self.assertTrue(Path(result["result"]["cacheFolder"]).is_dir())
 
-    def testRemotes(self):
+    def test_remotes(self):
         with Popen([str(LCI_BIN)], stdout=subprocess.PIPE, stdin=subprocess.PIPE) as proc:
-            outs, errs = proc.communicate(input=COMMAND_REMOTES.encode())
+            outs, errs = proc.communicate(input=COMMAND_REMOTES.format().encode())
             result = json.loads(outs.decode())
-            self.assertIsNone(result.get('error'))
-            self.assertTrue(isinstance(result['result'], dict))
-            self.assertEqual(3, len(result['result']))
+            self.assertIsNone(result.get("error"))
+            self.assertTrue(isinstance(result["result"], dict))
+            self.assertEqual(3, len(result["result"]))
 
-    def testPackages(self):
+    def test_packages(self):
         with Popen([str(LCI_BIN)], stdout=subprocess.PIPE, stdin=subprocess.PIPE) as proc:
-            outs, errs = proc.communicate(input=COMMAND_PACKAGES.encode())
+            outs, errs = proc.communicate(input=COMMAND_PACKAGES.format().encode())
             result = json.loads(outs.decode())
-            self.assertIsNone(result.get('error'))
-            self.assertTrue(isinstance(result['result'], dict))
-            self.assertEqual(len(result['result']), 2)
-            self.assertTrue(isinstance(result['result']['installedPackages'], dict))
-            self.assertTrue(isinstance(result['result']['availablePackages'], dict))
-            self.assertGreater(len(result['result']['installedPackages']), 1)
-            self.assertGreater(len(result['result']['availablePackages']), 1)
-
-        with Popen([str(LCI_BIN)], stdout=subprocess.PIPE, stdin=subprocess.PIPE) as proc:
-            outs, errs = proc.communicate(input=COMMAND_PACKAGES_AVAILABLE.encode())
-            result = json.loads(outs.decode())
-            self.assertIsNone(result.get('error'))
-            self.assertTrue(isinstance(result['result'], dict))
-            self.assertEqual(len(result['result']), 1)
-            self.assertTrue(isinstance(result['result']['availablePackages'], dict))
-            self.assertGreater(len(result['result']['availablePackages']), 1)
+            self.assertIsNone(result.get("error"))
+            self.assertTrue(isinstance(result["result"], dict))
+            self.assertEqual(len(result["result"]), 2)
+            self.assertTrue(isinstance(result["result"]["installedPackages"], dict))
+            self.assertTrue(isinstance(result["result"]["availablePackages"], dict))
+            self.assertGreater(len(result["result"]["installedPackages"]), 1)
+            self.assertGreater(len(result["result"]["availablePackages"]), 1)
 
         with Popen([str(LCI_BIN)], stdout=subprocess.PIPE, stdin=subprocess.PIPE) as proc:
-            outs, errs = proc.communicate(input=COMMAND_PACKAGES_INSTALLED.encode())
+            outs, errs = proc.communicate(input=COMMAND_PACKAGES_AVAILABLE.format().encode())
             result = json.loads(outs.decode())
-            self.assertIsNone(result.get('error'))
-            self.assertTrue(isinstance(result['result'], dict))
-            self.assertEqual(len(result['result']), 1)
-            self.assertTrue(isinstance(result['result']['installedPackages'], dict))
-            self.assertGreater(len(result['result']['installedPackages']), 1)
-
-    def testWorkspace(self):
-        with Popen([str(LCI_BIN)], stdout=subprocess.PIPE, stdin=subprocess.PIPE) as proc:
-            outs, errs = proc.communicate(input=COMMAND_WORKSPACE.encode())
-            result = json.loads(outs.decode())
-            self.assertIsNotNone(result.get('error'))
-            self.assertIsInstance(result.get('error'), dict)
-            self.assertIsInstance(result.get('error').get('type'), str)
-            self.assertIsInstance(result.get('error').get('message'), str)
+            self.assertIsNone(result.get("error"))
+            self.assertTrue(isinstance(result["result"], dict))
+            self.assertEqual(len(result["result"]), 1)
+            self.assertTrue(isinstance(result["result"]["availablePackages"], dict))
+            self.assertGreater(len(result["result"]["availablePackages"]), 1)
 
         with Popen([str(LCI_BIN)], stdout=subprocess.PIPE, stdin=subprocess.PIPE) as proc:
-            outs, errs = proc.communicate(input=(COMMAND_WORKSPACE__ws % '/foo/bar').encode())
+            outs, errs = proc.communicate(input=COMMAND_PACKAGES_INSTALLED.format().encode())
             result = json.loads(outs.decode())
-            self.assertIsNone(result.get('error'))
-            self.assertFalse(result['result']['initialized'])
+            self.assertIsNone(result.get("error"))
+            self.assertTrue(isinstance(result["result"], dict))
+            self.assertEqual(len(result["result"]), 1)
+            self.assertTrue(isinstance(result["result"]["installedPackages"], dict))
+            self.assertGreater(len(result["result"]["installedPackages"]), 1)
+
+    def test_workspace(self):
+        with Popen([str(LCI_BIN)], stdout=subprocess.PIPE, stdin=subprocess.PIPE) as proc:
+            outs, errs = proc.communicate(input=COMMAND_WORKSPACE.format().encode())
+            result = json.loads(outs.decode())
+            self.assertIsNotNone(result.get("error"))
+            self.assertIsInstance(result.get("error"), dict)
+            self.assertIsInstance(result.get("error").get("type"), str)
+            self.assertIsInstance(result.get("error").get("message"), str)
+
+        with Popen([str(LCI_BIN)], stdout=subprocess.PIPE, stdin=subprocess.PIPE) as proc:
+            outs, errs = proc.communicate(input=(COMMAND_WORKSPACE__ws.format(ws="/foo/bar")).encode())
+            result = json.loads(outs.decode())
+            self.assertIsNone(result.get("error"))
+            self.assertFalse(result["result"]["initialized"])
 
         workspace = os.getenv("LEAF_TEST_WORKSPACE")
         self.assertIsNotNone(workspace)
         with Popen([str(LCI_BIN)], stdout=subprocess.PIPE, stdin=subprocess.PIPE) as proc:
-            outs, errs = proc.communicate(input=(COMMAND_WORKSPACE__ws % workspace).encode())
+            outs, errs = proc.communicate(input=(COMMAND_WORKSPACE__ws.format(ws=workspace)).encode())
             result = json.loads(outs.decode())
-            self.assertIsNone(result.get('error'))
-            self.assertTrue(result['result']['initialized'])
-            self.assertEqual(workspace, result['result']['rootFolder'])
-            self.assertEqual(1, len(result['result']['profiles']))
+            self.assertIsNone(result.get("error"))
+            self.assertTrue(result["result"]["initialized"])
+            self.assertEqual(workspace, result["result"]["rootFolder"])
+            self.assertEqual(1, len(result["result"]["profiles"]))
 
-    def testVariables(self):
+    def test_variables(self):
         workspace = os.getenv("LEAF_TEST_WORKSPACE")
         self.assertIsNotNone(workspace)
         with Popen([str(LCI_BIN)], stdout=subprocess.PIPE, stdin=subprocess.PIPE) as proc:
-            outs, errs = proc.communicate(input=(COMMAND_VARIABLES__ws % workspace).encode())
+            outs, errs = proc.communicate(input=(COMMAND_VARIABLES__ws.format(ws=workspace)).encode())
             result = json.loads(outs.decode())
-            self.assertIsNone(result.get('error'))
-            self.assertTrue(len(result['result']) > 10)
-            self.assertEqual("192.168.2.2", result['result']['DEST_IP'])
-            self.assertEqual("wp76xx", result['result']['LEGATO_TARGET'])
-            self.assertFalse('FOO' in result['result'])
+            self.assertIsNone(result.get("error"))
+            self.assertTrue(len(result["result"]) > 10)
+            self.assertEqual("192.168.2.2", result["result"]["DEST_IP"])
+            self.assertEqual("wp76xx", result["result"]["LEGATO_TARGET"])
+            self.assertFalse("FOO" in result["result"])
         with Popen([str(LCI_BIN)], stdout=subprocess.PIPE, stdin=subprocess.PIPE) as proc:
-            outs, errs = proc.communicate(input=(COMMAND_VARIABLES__ws_name % (workspace, "SWI-WP76")).encode())
+            outs, errs = proc.communicate(input=(COMMAND_VARIABLES__ws_name.format(ws=workspace, key="SWI-WP76")).encode())
             result = json.loads(outs.decode())
-            self.assertIsNone(result.get('error'))
-            self.assertTrue(len(result['result']) > 10)
-            self.assertEqual("192.168.2.2", result['result']['DEST_IP'])
-            self.assertEqual("wp76xx", result['result']['LEGATO_TARGET'])
-            self.assertFalse('FOO' in result['result'])
+            self.assertIsNone(result.get("error"))
+            self.assertTrue(len(result["result"]) > 10)
+            self.assertEqual("192.168.2.2", result["result"]["DEST_IP"])
+            self.assertEqual("wp76xx", result["result"]["LEGATO_TARGET"])
+            self.assertFalse("FOO" in result["result"])
         with Popen([str(LCI_BIN)], stdout=subprocess.PIPE, stdin=subprocess.PIPE) as proc:
-            outs, errs = proc.communicate(input=(COMMAND_VARIABLES__ws_name % (workspace, "FOOO")).encode())
+            outs, errs = proc.communicate(input=(COMMAND_VARIABLES__ws_name.format(ws=workspace, key="FOOO")).encode())
             result = json.loads(outs.decode())
-            self.assertEqual("InvalidProfileNameException", result.get('error').get('type'))
-            self.assertEqual("Unknown profile FOOO", result.get('error').get('message'))
-            self.assertFalse('result' in result)
+            self.assertEqual("InvalidProfileNameException", result.get("error").get("type"))
+            self.assertEqual("Unknown profile FOOO", result.get("error").get("message"))
+            self.assertFalse("result" in result)
         with Popen([str(LCI_BIN)], stdout=subprocess.PIPE, stdin=subprocess.PIPE) as proc:
-            outs, errs = proc.communicate(input=(COMMAND_VARIABLES__ws_var % (workspace, 'DEST_IP')).encode())
+            outs, errs = proc.communicate(input=(COMMAND_VARIABLES__ws_var.format(ws=workspace, key="DEST_IP")).encode())
             result = json.loads(outs.decode())
-            self.assertIsNone(result.get('error'))
-            self.assertEqual(1, len(result['result']))
-            self.assertEqual("192.168.2.2", result['result']['DEST_IP'])
+            self.assertIsNone(result.get("error"))
+            self.assertEqual(1, len(result["result"]))
+            self.assertEqual("192.168.2.2", result["result"]["DEST_IP"])
         with Popen([str(LCI_BIN)], stdout=subprocess.PIPE, stdin=subprocess.PIPE) as proc:
-            outs, errs = proc.communicate(input=(COMMAND_VARIABLES__ws_var % (workspace, 'FOO')).encode())
+            outs, errs = proc.communicate(input=(COMMAND_VARIABLES__ws_var.format(ws=workspace, key="FOO")).encode())
             result = json.loads(outs.decode())
-            self.assertIsNone(result.get('error'))
-            self.assertEqual(1, len(result['result']))
-            self.assertEqual(None, result['result']['FOO'])
+            self.assertIsNone(result.get("error"))
+            self.assertEqual(1, len(result["result"]))
+            self.assertEqual(None, result["result"]["FOO"])
